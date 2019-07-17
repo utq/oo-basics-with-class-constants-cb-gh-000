19e430f697bab85c7ffeb943275e0982290a1223
Celal Cankaya <celalcankaya@outlook.com>
2019-07-17 19:51:36 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,7 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
 
   BRANDS = []
-
-  def initialize(brand)
-    @brand = brand
-  end
 
   def cobble
     self.condition = "new"
